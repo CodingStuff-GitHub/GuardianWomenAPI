9f99ba649c0a835ac3657b854245ea987aba0815
CodingStuff-Github <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-04-20 20:12:34 +0530

    No JWT

--- routes/guardian.js
+++ routes/guardian.js
@@ -5,56 +5,21 @@ const bcryptjs = require('bcryptjs');
 const user_jwt = require('../middleware/user_jwt');
 const jwt = require('jsonwebtoken');
 
-
-// router.get('/', user_jwt, async(req, res, next) => {
-//     try {
-//
-//         const user = await Guardian.findById(req.user.id).select('-password');
-//             res.status(200).json({
-//                 success: true,
-//                 user: user
-//             });
-//     } catch(error) {
-//         console.log(error.message);
-//         res.status(500).json({
-//             success: false,
-//             msg: 'Server Error.'
-//         })
-//         next();
-//     }
-// });
-
 router.post('/add', async (req, res, next) => {
     const {guardian_name, user_email, guardian_phone} = req.body;
 
     try {
         let guardian = new Guardian();
-
         guardian.guardian_name = guardian_name;
         guardian.user_email = user_email;
         guardian.guardian_phone = guardian_phone;
 
         await guardian.save();
 
-        const payload = {
-            guardian: {
-                id: guardian.id
-            }
-        }
-
-
-        jwt.sign(payload, process.env.jwtUserSecret, {
-            expiresIn: 360000
-        }, (err, token) => {
-            if (err) throw err;
-
-            res.status(200).json({
-                success: true,
-                guardian_token: token
-            });
+        res.status(200).json({
+            success: true,
+            guardian_token: token
         });
-
-
     } catch
         (err) {
         console.log(err);
